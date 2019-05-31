87f1e5d22b42a8cb52e883bc9c6d6ab9fa7d8517
Jon David <Jondavid_x@hotmail.com>
2019-05-31 00:17:06 -0400

    initialized FB & Firestore

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import firebase from 'firebase'
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyD5zrqbOKdUCuzBbqc5_2BXX1fgfi45BI8",
+    authDomain: "vuefirechat-4d09d.firebaseapp.com",
+    databaseURL: "https://vuefirechat-4d09d.firebaseio.com",
+    projectId: "vuefirechat-4d09d",
+    storageBucket: "vuefirechat-4d09d.appspot.com",
+    messagingSenderId: "402484501831",
+    appId: "1:402484501831:web:85f25ac84e4add30"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+
+window.db= db;
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h3>Plase Login with your google account to continue</h3>
+    <button @click="login">Login with google</button>
+  </div>
+</template>
+
+<script>
+import firebase, { functions } from 'firebase';
+export default {
+    methods:{
+        login(){
+            var provider= new firebase.auth.GoogleAuthProvider(); 
+       
+       
+       firebase.auth().signInWithPopup(provider).then(result =>{
+           var token = result.credential.accessToken;
+           var user = result.user;
+
+this.$router.push('/')
+
+       }).catch(function(error) {
+           var errorCode = error.code;
+           var errorMessage= error.message;
+           var email = error.email;
+           var credential = error.credential;
+
+       })
+       }
+
+       }
+    }
+
+</script>
+
+
